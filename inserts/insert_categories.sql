5e8e89ea096f074cc7cdb333c1b50f915fc57383
Mauro Brandan <maurobrandan.mb@gmail.com>
2022-03-12 16:14:14 -0300

    Datos de pruebas

---
+++ inserts/insert_categories.sql
@@ -0,0 +1,5 @@
+INSERT INTO `blog`.`categorias` (`id`,`nombre_categoria`) VALUES (1,'Ciencia');
+INSERT INTO `blog`.`categorias` (`id`,`nombre_categoria`) VALUES (2,'Tecnología');
+INSERT INTO `blog`.`categorias` (`id`,`nombre_categoria`) VALUES (3,'Deportes');
+INSERT INTO `blog`.`categorias` (`id`,`nombre_categoria`) VALUES (4,'Espectáculos');
+INSERT INTO `blog`.`categorias` (`id`,`nombre_categoria`) VALUES (5,'Economía');
---
+++ inserts/insert_users.sql
@@ -0,0 +1,5 @@
+INSERT INTO `blog`.`usuarios` (`id`,`login`,`password`,`nickname`,`email`) VALUES (1,'israel','jc8209*(^GCHN_(hcLA','Israel','israel@blog.com');
+INSERT INTO `blog`.`usuarios` (`id`,`login`,`password`,`nickname`,`email`) VALUES (2,'monica','(*&^LKJDHC_(*#YDLKJHODG','Moni','monica@blog.com');
+INSERT INTO `blog`.`usuarios` (`id`,`login`,`password`,`nickname`,`email`) VALUES (3,'laura','LKDJ)_*(-c.M:\"[pOwHDˆåßƒ∂','Lau','laura@blog.com');
+INSERT INTO `blog`.`usuarios` (`id`,`login`,`password`,`nickname`,`email`) VALUES (4,'edgar','LLiy)CX*Y:M<A<SC_(*N>O','Ed','edgar@blog.com');
+INSERT INTO `blog`.`usuarios` (`id`,`login`,`password`,`nickname`,`email`) VALUES (5,'perezoso','&N_*JS)_Y)*(&TGOKS','Oso Pérez','perezoso@blog.com');
